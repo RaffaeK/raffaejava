13d338ee4b11ee72da0bab740da2ea7b6197efd9
Anum Qudsia <anumqudsia@hotmail.com>
2019-05-07 21:11:58 +0200

    Changed from Menu to JMenu components.

--- src/obj2100/MainWindow.java
+++ src/obj2100/MainWindow.java
@@ -1,106 +1,114 @@
 package obj2100;
-import java.awt.event.ActionListener;
-import java.awt.GridLayout;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.MenuItem;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Frame;
-import java.util.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainWindow extends JFrame implements ActionListener {
-	
-	
+
 	SearchCard previousPanel = null;
-	SearchCard currentPanel = null; 
+	SearchCard currentPanel = null;
 	ArrayList<SearchCard> listOfCards = null;
 	MainMenu mainMenu = null;
 	int number = 0;
+
 	public MainWindow() {
 		this.setTitle("PDFsearch");
 		this.setSize(1000, 600);
-		this.setVisible(true);
 		this.setLayout(new BorderLayout());
-		
+
 		// setter vinduet i midten//
 		this.setLocationRelativeTo(null);
-		
-		
-		mainMenu = new MainMenu();
-		
-		setMenuBar(mainMenu.getMenuBar());
-	      
-		mainMenu.getCloseButton().addActionListener(this);
-	    mainMenu.getAddCardButton().addActionListener(this);
-	      
-	      addWindowListener(new WindowAdapter() {
-			  public void windowClosing(WindowEvent e) {
-				  System.exit(0); }
-		      });
-	      
-	      listOfCards = new ArrayList<SearchCard>();
-	 
+
+		this.getContentPane().setBackground(Color.cyan);
+
+		this.setMenuBar();
+
+		listOfCards = new ArrayList<SearchCard>();
+
+		this.setVisible(true);
+
 	}
+
+	public void setMenuBar() {
+
+		mainMenu = new MainMenu();
+		this.setJMenuBar(mainMenu.getJMenuBar());
+
+		mainMenu.getCloseButton().addActionListener(this);
+		mainMenu.getAddCardButton().addActionListener(this);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
 	public void actionPerformed(ActionEvent evt) {
-		 String arg = evt.getActionCommand();
-		  if(arg.equals("Avslutt")) System.exit(0);
-		  if(arg.equals("Lag et nytt kort")) {
+		String arg = evt.getActionCommand();
+		if (arg.equals("Avslutt"))
+			System.exit(0);
+		if (arg.equals("Lag et nytt kort")) {
 
-			  //hvis currentPanel er satt betyr det at vi må skjule det før vi viser den neste
-			  if ( currentPanel != null ) {
-				  currentPanel.setVisible(false);
-			  }
-			  
-			  SearchCard newSearchCard = new SearchCard("KortNummer " + number++);
-			  currentPanel = newSearchCard;
-			  currentPanel.setVisible(true);
-		      this.add(newSearchCard, BorderLayout.CENTER);
-		      
-		      listOfCards.add(newSearchCard);
-		      
-		      MenuItem menuItem = new MenuItem(newSearchCard.getTitle());
-		      menuItem.addActionListener(new ActionListener() {
-		          public void actionPerformed(ActionEvent e) {
+			// hvis currentPanel er satt betyr det at vi må skjule det før vi viser den
+			// neste
+			if (currentPanel != null) {
+				currentPanel.setVisible(false);
+			}
 
-		        	  //switch the damn panel
-		        	  // which panel is it? 
-		        	  
-		        	  Integer index = getIndexOfSwitchedCard(newSearchCard);
-		        	  SearchCard searchCardClicked = listOfCards.get(index);
-		              System.out.println(searchCardClicked.getTitle() + " clicked");
-		              
-		             //hide the previous search
-		    		  if ( currentPanel != null ) {
-						  currentPanel.setVisible(false);
-					  }
-		    		  
-		    		  currentPanel = searchCardClicked;
-				      currentPanel.setVisible(true);
-		    		  
-		        	 
-		          }
+			SearchCard newSearchCard = new SearchCard("KortNummer " + number++);
+			currentPanel = newSearchCard;
+			currentPanel.setVisible(true);
+			this.add(newSearchCard, BorderLayout.CENTER);
+
+			listOfCards.add(newSearchCard);
+
+			JMenuItem menuItem = new JMenuItem(newSearchCard.getTitle());
+			menuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+
+					// switch the damn panel
+					// which panel is it?
+
+					Integer index = getIndexOfSwitchedCard(newSearchCard);
+					SearchCard searchCardClicked = listOfCards.get(index);
+					System.out.println(searchCardClicked.getTitle() + " clicked");
+
+					// hide the previous search
+					if (currentPanel != null) {
+						currentPanel.setVisible(false);
+					}
+
+					currentPanel = searchCardClicked;
+					currentPanel.setVisible(true);
+
+				}
 
 				private Integer getIndexOfSwitchedCard(SearchCard searchCard) {
 					String t = searchCard.getTitle();
-		        	  String lastChar = t.substring(t.length() - 1);
-		        	  Integer index = Integer.parseInt(lastChar);
+					String lastChar = t.substring(t.length() - 1);
+					Integer index = Integer.parseInt(lastChar);
 					return index;
 				}
-		      });
-		      
-		      mainMenu.addMenuItem(menuItem);
-		      this.revalidate();
-		      this.repaint();
-		}
-	   
-	   }
-	
+			});
 
-		 
+			mainMenu.addMenuItem(menuItem);
+			this.revalidate();
+			this.repaint();
+		}
+
 	}
 
+}
